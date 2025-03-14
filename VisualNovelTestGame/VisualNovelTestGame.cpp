969fd31cb2abbe3e0b3c06edf82dd20fe2337776
Leshen Naicker <leshenn0106@gmail.com>
2025-03-14 11:26:26 +0200

    Add project files.

---
+++ VisualNovelTestGame/VisualNovelTestGame.cpp
@@ -0,0 +1,72 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+using namespace sf;
+using namespace std;
+const int WIN_WIDTH = 890;
+const int WIN_HEIGHT = 448;
+
+int main()
+{
+	RenderWindow window(VideoMode(WIN_WIDTH, WIN_HEIGHT), "Visual Novel");
+	Texture background;
+	Texture MainCharacter;
+	Texture Zeus;
+	Texture scroll;
+	Font font;
+
+	font.loadFromFile("Robot_Font.otf");
+	Text dialogText;
+	dialogText.setFont(font);
+	dialogText.setString("Test Text");
+	dialogText.setCharacterSize(24);
+	dialogText.setFillColor(Color::Black);
+	dialogText.setPosition(400, 375);
+
+	ifstream file("test.json");
+	json poesidonData;
+	file >> poesidonData;
+	vector<json> phraseDialog = poesidonData["poesidon"];
+
+	MainCharacter.loadFromFile("MainCharacter.png");
+	Sprite characterSprite(MainCharacter);
+	characterSprite.setPosition(0, 220);
+
+	Zeus.loadFromFile("zeus.png");
+	Sprite zeusSprite(Zeus);
+	zeusSprite.setPosition(620, 220);
+
+	background.loadFromFile("Coliseum.jpg");
+	Sprite backgroundSprite(background);
+	backgroundSprite.setScale(0.5, 0.5);
+
+	scroll.loadFromFile("scroll.png");
+	Sprite scrollSprite(scroll);
+	scrollSprite.setScale(0.35, 0.35);
+	scrollSprite.setPosition(153, 300);
+
+
+	while (window.isOpen()) {
+
+		Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+
+		window.clear();
+		window.draw(backgroundSprite);
+		window.draw(scrollSprite);
+		window.draw(zeusSprite);
+		window.draw(characterSprite);
+		window.draw(dialogText);
+		
+		
+		window.display();
+	}
+}
