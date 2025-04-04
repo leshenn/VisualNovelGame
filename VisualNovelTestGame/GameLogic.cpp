96d83bc6215e1c78f6cec8217c600519d37b020d
Leshen Naicker <leshenn0106@gmail.com>
2025-04-04 14:17:13 +0200

    Updated game logic

--- VisualNovelTestGame/GameLogic.cpp
+++ VisualNovelTestGame/GameLogic.cpp
@@ -6,74 +6,70 @@
 
 using namespace sf;
 
-void handleGameLogic(RenderWindow& window, GameState& currentState, ButtonLayout& layout, LoadSprites& loadSprites, Event& event, Audio& audio)
-{   
+void loadGameAssets(GameState currentState, LoadSprites& loadSprites)
+{
     switch (currentState) {
     case GameState::MENU:
-        if (event.type == Event::MouseButtonPressed) {
-            audio.playClickButtonSound();
-            if (layout.playButtonClicked(window)) {
-                currentState = GameState::INTRO; // Change state when Play button is clicked
-
-            }
-        }
-
-        // Show the necessary sprites and buttons
-        window.clear();
-
-        // load in necessary sprites
         loadSprites.loadMenuScreen("Backgrounds/MenuBackground.png");
+        break;
 
+    case GameState::INTRO:
+        loadSprites.loadInstructionScreen("Backgrounds/InstructionBackground.jpg", "Acessories/MenuScroll.png");
+        break;
+
+    case GameState::STAGE_ONE_MENU:
+        loadSprites.loadMenuScreen("Backgrounds/MenuBackground00.png");
+        break;
+
+    case GameState::SEAWORLD:
+        loadSprites.loadGameScreen("Backgrounds/PoseidonBackground.jpg", "Characters/Poseidon.png", "Characters/MainCharacter.png", "Acessories/Scroll.png");
+        break;
+
+    case GameState::UNDERWORLD:
+        loadSprites.loadGameScreen("Backgrounds/HadesBackground.jpg", "Characters/Hades.png", "Characters/MainCharacter.png", "Acessories/Scroll.png");
+        break;
+
+    case GameState::COUNTRY_SIDE:
+        loadSprites.loadGameScreen("Backgrounds/DionysusBackground.jpg", "Characters/Dionysus.png", "Characters/MainCharacter.png", "Acessories/Scroll.png");
+        break;
+
+    default:
+        break;
+    }
+}
+
+void renderGameScene(RenderWindow& window, GameState currentState, ButtonLayout& layout, LoadSprites& loadSprites)
+{
+    switch (currentState) {
+    case GameState::MENU:
+        window.clear();
         window.draw(loadSprites.menuBackgroundSprite);
         layout.loadPlayButton();
         break;
 
     case GameState::INTRO:
-        if (event.type == Event::MouseButtonPressed) {
-            audio.playClickButtonSound();
-            if (layout.nextButtonClicked(window)) {
-                currentState = GameState::STAGE_ONE_MENU;
-            }
-        }
-
-        // Show the necessary sprites and buttons
         window.clear();
-
-        // load ncecessary sprites
-        loadSprites.loadInstructionScreen("Backgrounds/InstructionBackground.jpg", "Acessories/MenuScroll.png");
-
         window.draw(loadSprites.instructionBackgroundSprite);
         window.draw(loadSprites.menuScrollSprite);
         layout.loadNextButton();
         break;
 
     case GameState::STAGE_ONE_MENU:
-        audio.playClickButtonSound();
-        if (event.type == Event::MouseButtonPressed) {
-            Vector2i mousePos = Mouse::getPosition(window);
-            currentState = layout.loadButtonClicked(mousePos);
-        }
-
         window.clear();
-        loadSprites.loadMenuScreen("Backgrounds/MenuBackground00.png");
         window.draw(loadSprites.menuBackgroundSprite);
         layout.loadWorldButtons();
         break;
 
     case GameState::SEAWORLD:
         window.clear();
-        loadSprites.loadGameScreen("Backgrounds/PoseidonBackground.jpg", "Characters/Poseidon.png", "Characters/MainCharacter.png", "Acessories/Scroll.png");
         window.draw(loadSprites.gameBackgroundSprite);
         window.draw(loadSprites.godSprite);
         window.draw(loadSprites.mainCharacterSprite);
         window.draw(loadSprites.gameScrollSprite);
-
-
         break;
 
     case GameState::UNDERWORLD:
         window.clear();
-        loadSprites.loadGameScreen("Backgrounds/HadesBackground.jpg", "Characters/Hades.png", "Characters/MainCharacter.png", "Acessories/Scroll.png");
         window.draw(loadSprites.gameBackgroundSprite);
         window.draw(loadSprites.godSprite);
         window.draw(loadSprites.mainCharacterSprite);
@@ -82,7 +78,6 @@ void handleGameLogic(RenderWindow& window, GameState& currentState, ButtonLayout
 
     case GameState::COUNTRY_SIDE:
         window.clear();
-        loadSprites.loadGameScreen("Backgrounds/DionysusBackground.jpg", "Characters/Dionysus.png", "Characters/MainCharacter.png", "Acessories/Scroll.png");
         window.draw(loadSprites.gameBackgroundSprite);
         window.draw(loadSprites.godSprite);
         window.draw(loadSprites.mainCharacterSprite);
@@ -94,4 +89,51 @@ void handleGameLogic(RenderWindow& window, GameState& currentState, ButtonLayout
     }
 
     window.display();  // Update the window with the newly drawn content
+}
+
+void handleGameLogic(RenderWindow& window, GameState& currentState, ButtonLayout& layout, LoadSprites& loadSprites, Event& event, Audio& audio)
+{
+    switch (currentState) {
+    case GameState::MENU:
+        if (event.type == Event::MouseButtonPressed) {
+            audio.playClickButtonSound();
+            if (layout.playButtonClicked(window)) {
+                currentState = GameState::INTRO; // Change state when Play button is clicked
+            }
+        }
+        loadGameAssets(currentState, loadSprites);
+        renderGameScene(window, currentState, layout, loadSprites);
+        break;
+
+    case GameState::INTRO:
+        if (event.type == Event::MouseButtonPressed) {
+            audio.playClickButtonSound();
+            if (layout.nextButtonClicked(window)) {
+                currentState = GameState::STAGE_ONE_MENU;
+            }
+        }
+        loadGameAssets(currentState, loadSprites);
+        renderGameScene(window, currentState, layout, loadSprites);
+        break;
+
+    case GameState::STAGE_ONE_MENU:
+        audio.playClickButtonSound();
+        if (event.type == Event::MouseButtonPressed) {
+            Vector2i mousePos = Mouse::getPosition(window);
+            currentState = layout.loadButtonClicked(mousePos);
+        }
+        loadGameAssets(currentState, loadSprites);
+        renderGameScene(window, currentState, layout, loadSprites);
+        break;
+
+    case GameState::SEAWORLD:
+    case GameState::UNDERWORLD:
+    case GameState::COUNTRY_SIDE:
+        loadGameAssets(currentState, loadSprites);
+        renderGameScene(window, currentState, layout, loadSprites);
+        break;
+
+    default:
+        break;
+    }
 }
--- VisualNovelTestGame/GameLogic.h
+++ VisualNovelTestGame/GameLogic.h
@@ -8,5 +8,9 @@
 
 using namespace sf;
 
+void loadGameAssets(GameState currentState, LoadSprites& loadSprites);
+
+void renderGameScene(RenderWindow& window, GameState currentState, ButtonLayout& layout, LoadSprites& loadSprites);
+
 void handleGameLogic(RenderWindow& window, GameState& currentState, 
 	ButtonLayout& layout, LoadSprites& loadSprites, Event& event, Audio& audio);
